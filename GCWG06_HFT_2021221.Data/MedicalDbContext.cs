1e2bef89a2d77f7694c6c60cef8f65392e34b424
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-10-15 01:22:01 +0200

    making the db but implementing and other things are still missing

--- GCWG06_HFT_2021221.Data/MedicalDbContext.cs
+++ GCWG06_HFT_2021221.Data/MedicalDbContext.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GCWG06_HFT_2021221.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GCWG06_HFT_2021221.Data
 {
     class MedicalDbContext : DbContext
     {
-        //public virtual DbSet<Hospital> Hospitals { get; set; }
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        //employees dbset also needed
+        public MedicalDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
