6cf7c9bc6e16d7ed639b7d458f4db2774a5f85ac
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-10-15 00:37:00 +0200

    database settings

---
+++ GCWG06_HFT_2021221.Data/MedicalDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GCWG06_HFT_2021221.Data
+{
+    class MedicalDbContext : DbContext
+    {
+        //public virtual DbSet<Hospital> Hospitals { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MedicalDb.mdf;Integrated Security=True");
+            }
+        }
+    }
+}
