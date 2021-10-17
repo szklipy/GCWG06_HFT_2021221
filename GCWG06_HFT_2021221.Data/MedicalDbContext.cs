2739e514d990891df5eec43cb249e9639a10c358
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-10-17 20:07:15 +0200

    finishing implementation

--- GCWG06_HFT_2021221.Data/MedicalDbContext.cs
+++ GCWG06_HFT_2021221.Data/MedicalDbContext.cs
@@ -33,31 +33,34 @@ namespace GCWG06_HFT_2021221.Data
                 Hospital_location = "Budapest"
             };
 
-            Department d1 = new Department() { Department_id = 1, Department_name = "", Hospital_id = 1 };
-            Department d2 = new Department() { Department_id = 2, Department_name = "", Hospital_id = null };
-            Department d3 = new Department() { Department_id = 3, Department_name = "", Hospital_id = 1 };
-            Department d4 = new Department() { Department_id = 4, Department_name = "", Hospital_id = 1 };
-            Department d5 = new Department() { Department_id = 5, Department_name = "", Hospital_id = 1 };
-            Department d6 = new Department() { Department_id = 6, Department_name = "", Hospital_id = 1 };
+            //modelbuilders
+            //one-many many-many one-one many-one
+ Department d1 = new Department() { Department_id = 1, Department_name = "Internal Medicine Infectology", Hospital_id = 1 };
+            Department d2 = new Department() { Department_id = 2, Department_name = "Hospice department", Hospital_id = null };
+            Department d3 = new Department() { Department_id = 3, Department_name = "Central Operating Room", Hospital_id = 1 };
+            Department d4 = new Department() { Department_id = 4, Department_name = "Pathology", Hospital_id = 1 };
+            Department d5 = new Department() { Department_id = 5, Department_name = "Department of Urology", Hospital_id = 1 };
+            Department d6 = new Department() { Department_id = 6, Department_name = "Department of Infectious Diseases", Hospital_id = 1 };
 
-            Employee e1 = new Employee() { Employee_id = 1, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 1 };
-            Employee e2 = new Employee() { Employee_id = 2, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 3 };
-            Employee e3 = new Employee() { Employee_id = 3, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 3 };
-            Employee e4 = new Employee() { Employee_id = 4, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 4 };
-            Employee e5 = new Employee() { Employee_id = 5, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 4 };
-            Employee e6 = new Employee() { Employee_id = 6, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 5 };
-            Employee e7 = new Employee() { Employee_id = 7, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 4 };
-            Employee e8 = new Employee() { Employee_id = 8, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 1 };
-            Employee e9 = new Employee() { Employee_id = 9, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 5 };
-            Employee e10 = new Employee() { Employee_id = 10, Name = "", Job_title = "", Hire_date = "", Hospital_id = null, Department_id = null };
-            Employee e11 = new Employee() { Employee_id = 11, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 4 };
-            Employee e12 = new Employee() { Employee_id = 12, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 3 };
-            Employee e13 = new Employee() { Employee_id = 13, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 1 };
-            Employee e14 = new Employee() { Employee_id = 14, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 4 };
-            Employee e15 = new Employee() { Employee_id = 15, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 6 };
-            Employee e16 = new Employee() { Employee_id = 16, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 6 };
-            Employee e17 = new Employee() { Employee_id = 17, Name = "", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 1 };
+            Employee e1 = new Employee() { Employee_id = 1, Name = "Steven John", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 1 };
+            Employee e2 = new Employee() { Employee_id = 2, Name = "Margaret Jobs", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 3 };
+            Employee e3 = new Employee() { Employee_id = 3, Name = "Bob Dylan", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 3 };
+            Employee e4 = new Employee() { Employee_id = 4, Name = "Donald Trump", Job_title = "head physician", Hire_date = "", Hospital_id = 1, Department_id = 4 };
+            Employee e5 = new Employee() { Employee_id = 5, Name = "Hilary Clinton", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 4 };
+            Employee e6 = new Employee() { Employee_id = 6, Name = "Cameron Diaz", Job_title = "head physician", Hire_date = "", Hospital_id = 1, Department_id = 5 };
+            Employee e7 = new Employee() { Employee_id = 7, Name = "Bruce Willis", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 4 };
+            Employee e8 = new Employee() { Employee_id = 8, Name = "Tom Cruse", Job_title = "head physician", Hire_date = "", Hospital_id = 1, Department_id = 1 };
+            Employee e9 = new Employee() { Employee_id = 9, Name = "Bradley Cooper", Job_title = "nurse", Hire_date = "", Hospital_id = 1, Department_id = 5 };
+            Employee e10 = new Employee() { Employee_id = 10, Name = "Brad Pitt", Job_title = "nurse", Hire_date = "", Hospital_id = null, Department_id = null };
+            Employee e11 = new Employee() { Employee_id = 11, Name = "Steven Simons", Job_title = "nurse", Hire_date = "", Hospital_id = 1, Department_id = 4 };
+            Employee e12 = new Employee() { Employee_id = 12, Name = "John Wick", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 3 };
+            Employee e13 = new Employee() { Employee_id = 13, Name = "Jackey Chan", Job_title = "nurse", Hire_date = "", Hospital_id = 1, Department_id = 1 };
+            Employee e14 = new Employee() { Employee_id = 14, Name = "Lee Shang", Job_title = "doctor", Hire_date = "", Hospital_id = 1, Department_id = 4 };
+            Employee e15 = new Employee() { Employee_id = 15, Name = "Usain Bolt", Job_title = "", Hire_date = "", Hospital_id = 1, Department_id = 6 };
+            Employee e16 = new Employee() { Employee_id = 16, Name = "John Newmann", Job_title = "head physician", Hire_date = "", Hospital_id = 1, Department_id = 6 };
+            Employee e17 = new Employee() { Employee_id = 17, Name = "Albert Robinson", Job_title = "nurse", Hire_date = "", Hospital_id = 1, Department_id = 1 };
             
+
             //dep. (one-many) emp.
             //hosp. (one-many) emp.
             //hosp. (one-many) dep.
