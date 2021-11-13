faa728970aa2b6f34d563d53c6dac79b7d874736
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-11-13 01:13:47 +0100

    one base repo for the less writing

---
+++ GCWG06_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCWG06_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T :class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Delete(int id);
+    }
+}
