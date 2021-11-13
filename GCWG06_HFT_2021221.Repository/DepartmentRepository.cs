c14f3dfa20f0e630fcc77c3f00e635cf9bfc26f4
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-11-13 01:20:22 +0100

    making the repository... not sure about dep.name needs to be updated also not sure about save changes

---
+++ GCWG06_HFT_2021221.Repository/DepartmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using GCWG06_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCWG06_HFT_2021221.Repository
+{
+    public class DepartmentRepository :
+        Repository<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(DbContext ctx) : 
+            base(ctx) {}
+        public override Department GetOne(int id)
+        {
+            return GetAll()
+                .SingleOrDefault(x => x
+                .Department_id == id);
+        }
+        public void Create(Department department)
+        {
+            ctx.Set<Department>().Add(department);
+            ctx.SaveChanges();
+        }
+
+        public void Update(Department department)
+        {
+            var departmentToUpdate = GetOne(department.Department_id);
+            //biztos h ez kell?
+            departmentToUpdate.Department_name = department.Department_name;
+            departmentToUpdate.Employees = department.Employees;
+            departmentToUpdate.Hospital = department.Hospital;
+            //vajon kell e ide is ctx.SaveChanges(); ?
+        }
+    }
+}
