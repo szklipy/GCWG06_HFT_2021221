ae1537a1d3fc5a1ff09ed4037104a3210dd629ea
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-11-13 01:19:18 +0100

    making the repositories, not sure if update needs savechanges

---
+++ GCWG06_HFT_2021221.Repository/EmployeeRepository.cs
@@ -0,0 +1,39 @@
+﻿using GCWG06_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCWG06_HFT_2021221.Repository
+{
+    public class EmployeeRepository :
+        Repository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(DbContext ctx) : 
+            base(ctx) {}
+        public void Create(Employee employee)
+        {
+            ctx.Set<Employee>().Add(employee);
+            ctx.SaveChanges();
+        }
+
+        public override Employee GetOne(int id)
+        {
+            return GetAll()
+                .SingleOrDefault(x => x
+                .Employee_id == id);
+        }
+
+        public void Update(Employee employee)
+        {
+            var employeeToUpdate = GetOne(employee.Employee_id);
+            employeeToUpdate.Name = employee.Name;
+            employeeToUpdate.Job_title = employee.Job_title;
+            employeeToUpdate.Hospital = employee.Hospital;
+            employeeToUpdate.Department = employee.Department;
+            employeeToUpdate.Hire_date = employee.Hire_date;
+        }
+    }
+}
---
+++ GCWG06_HFT_2021221.Repository/HospitalRepository.cs
@@ -0,0 +1,37 @@
+﻿using GCWG06_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCWG06_HFT_2021221.Repository
+{
+    public class HospitalRepository :
+        Repository<Hospital>, IHospitalRepository
+    {
+        public HospitalRepository(DbContext ctx) :
+            base(ctx) {}
+        public override Hospital GetOne(int id)
+        {
+            return GetAll()
+                .SingleOrDefault(x => x
+                .Hospital_id == id);
+        }
+        public void Create(Hospital hospital)
+        {
+            ctx.Set<Hospital>().Add(hospital);
+            ctx.SaveChanges();
+        }
+
+        public void Update(Hospital hospital)
+        {
+            var hospitalToUpdate = GetOne(hospital.Hospital_id);
+            hospitalToUpdate.Hospital_name = hospital.Hospital_name;
+            hospitalToUpdate.Hospital_location = hospital.Hospital_location;
+            hospitalToUpdate.Employees = hospital.Employees;
+            hospitalToUpdate.Departments = hospital.Departments;
+        }
+    }
+}
