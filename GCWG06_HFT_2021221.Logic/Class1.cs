a566c5b3fcb59d352b29241047fe476fd21775eb
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-11-20 17:08:06 +0100

    editing, and preparing for the end of the world

--- GCWG06_HFT_2021221.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GCWG06_HFT_2021221.Logic
-{
-    public class Class1
-    {
-    }
-}
--- GCWG06_HFT_2021221.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GCWG06_HFT_2021221.Test
-{
-    public class Class1
-    {
-    }
-}
