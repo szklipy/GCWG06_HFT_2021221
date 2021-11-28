d1dddd8f2393278f62019f797de6a88e31c2f340
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2021-11-29 00:11:45 +0100

    hope it i didn't forgot anything

--- GCWG06_HFT_2021221.Client/Program.cs
+++ GCWG06_HFT_2021221.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GCWG06_HFT_2021221.Models;
+using System;
 
 
 namespace GCWG06_HFT_2021221.Client
@@ -7,8 +8,22 @@ namespace GCWG06_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            RestService restService = new RestService("http://localhost:51716");
 
+            restService.Post<Department>(new Department()
+            {
+                Department_name = "Patology"
+            }, "department");
+            restService.Post<Hospital>(new Hospital()
+            {
+                Hospital_name = "Honvéd"
+            }, "department");
+
+            var departments = restService.Get<Department>("department");
+            var employees = restService.Get<Employee>("employee");
+            var hospitals = restService.Get<Hospital>("hospital");
+            ;
         }
     }
 }
