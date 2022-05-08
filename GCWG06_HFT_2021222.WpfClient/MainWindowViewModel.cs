2cf06ba272af8e1000d968d2ac18b23ef72da3a5
David.Sipos@stud.uni-obuda.hu <David.Sipos@stud.uni-obuda.hu>
2022-05-08 22:29:50 +0200

    wpf kezdet

---
+++ GCWG06_HFT_2021222.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GCWG06_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCWG06_HFT_2021222.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Employee> Employees { get; set; }
+        public MainWindowViewModel()
+        {
+            Employees = new RestCollection<Employee>("http://localhost:16099/swagger/", "employee");
+
+        }
+    }
+}
